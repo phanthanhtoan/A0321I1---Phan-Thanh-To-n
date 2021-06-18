8b1a2c7bd7e15dfaa537910547e8e93e5f425e2b
phanthanhtoan <toan27122404@gmail.com>
2021-06-19 02:09:09 +0700

    baitap

---
+++ module02/src/bai1/baitap/thuchanh/thuchanh/th_thoigianhethong.java
@@ -0,0 +1,10 @@
+package bai1.baitap.thuchanh.thuchanh;
+
+import java.util.Date;
+
+public class th_thoigianhethong {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: "+now);
+    }
+}
