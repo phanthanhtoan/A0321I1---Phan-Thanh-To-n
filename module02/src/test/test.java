86b097c59a8543e2f2ab5f4d0ff0975686ffd1ec
A0321I1---Phan-Thanh-To-n <toan27122404@gmail.com>
2021-07-19 18:05:43 +0700

    123

--- module02/src/test/test.java
+++ module02/src/test/test.java
@@ -1,26 +1,2 @@
-package test;
 
-import java.util.Scanner;
 
-public class test {
-    public static void main(String[] args) {
-        int sum =0;
-        int[][] matrix = new int[2][2];
-        java.util.Scanner input = new Scanner(System.in);
-        System.out.println("Enter " + matrix.length + " rows and " +
-                matrix[0].length + " columns: ");
-        for (int row = 0; row < matrix.length; row++) {
-            for (int column = 0; column < matrix[row].length; column++) {
-                matrix[row][column] = (int) (Math.random() * 100);
-                sum += matrix[row][column];
-            }
-        }
-        for (int row = 0; row < matrix.length; row++) {
-            for (int column = 0; column < matrix[row].length; column++) {
-                System.out.print(matrix[row][column] +  " ");
-            }
-            System.out.println();
-
-        }
-    }
-}
